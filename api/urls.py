76f2cca4cb171e927eb367664c3ae03a9379b511
brocode <kelimbth@gmail.com>
2024-06-10 14:10:14 +0300

    created API endpoints

--- api/urls.py
+++ api/urls.py
@@ -1,1 +1,8 @@
+from django.urls import path
+from . import views
 
+
+urlpatterns = [
+    path('books', views.BooksCreateListView.as_view()),
+    path('books/<str:bookID>', views.BookDetailVieworDeleteBookView.as_view()),
+]
